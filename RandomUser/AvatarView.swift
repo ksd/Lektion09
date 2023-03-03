16376b19aec14428760d4a1c0f98165946af6b9f
Kaj Schermer Didriksen <kdidriksen@me.com>
2023-03-03 07:46:59 +0100

    Separated avatar into its own view

---
+++ RandomUser/AvatarView.swift
@@ -0,0 +1,31 @@
+//
+//  AvatarView.swift
+//  RandomUser
+//
+//  Created by ksd on 03/03/2023.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+    
+    let image: Image
+    var body: some View {
+        
+        image
+            .resizable()
+            .scaledToFit()
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 2)
+            }
+            .shadow(radius: 7)
+            .frame(width: 80, height: 80)
+    }
+}
+
+struct AvatarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvatarView(image: Image(systemName: "person"))
+    }
+}
